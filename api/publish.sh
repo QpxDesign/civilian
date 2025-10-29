37cc6cf9e0de4a691ae1aec2e5bcca26e231c4b6
qpxdesign <qpxdesign@users.noreply.github.com>
2025-10-29 16:28:38 -0700

    publish

--- api/publish.sh
+++ api/publish.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "publish"
 git push -u origin main
-ssh quinn@serverus "cd /home/quinn/projects/civilian/api; git fetch; git pull; docker build . -t civilian-api; cd ..; docker save civilian-api -o builds/civilian-api.tar; scp builds/civilian-api.tar root@100.87.34.34:/root/docker/nginx/sites/civilian/builds"
+ssh quinn@serverus "cd /home/quinn/projects/civilian/api; git fetch; git pull; DOCKER_BUILDKIT=1 docker build . -t civilian-api; cd ..; docker save civilian-api -o builds/civilian-api.tar; scp builds/civilian-api.tar root@100.87.34.34:/root/docker/nginx/sites/civilian/builds"
